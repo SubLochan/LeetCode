b87069e7a1ccbc7cb5e919a02888199559283963
Lochan <sublarilochan123@gmail.com>
2026-07-28 11:57:05 +0530

    Time: 0 ms (100%), Space: 8.6 MB (52%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,20 @@
+#include<cctype>
+class Solution {
+public:
+    string clearDigits(string s) {
+        string res = "";
+       for(char ch : s){
+            if(isdigit(ch)){
+                res.pop_back();
+            }
+            else{
+                res.push_back(ch);
+            }
+       }
+        return res;
+    }
+    
+    
+    
+    
+};
