f09c17e83188f1fc89deb6583b1dcb946e7f105c
Lochan <sublarilochan123@gmail.com>
2026-08-01 18:21:02 +0530

    Time: 0 ms (100%), Space: 17.4 MB (12.68%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -3,8 +3,13 @@ public:
     int countSeniors(vector<string>& details) {
         int c = 0;
         for(string ent : details){
-            string age = ent.substr(ent.size() - 4, 2);
+            int n = ent.size();
+            string age="";
+            age += ent[n-4];
+            age += ent[n-3];
+        
             int num = stoi(age);
+            
             if(num > 60){
                 c++;
             }
