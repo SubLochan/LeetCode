31b79f57351c056c6fd71f6823fda19dee81b3b1
Lochan <sublarilochan123@gmail.com>
2026-08-03 20:38:45 +0530

    Time: 0 ms (100%), Space: 10.4 MB (68.89%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int a = 0 , b = 0;
+        for(int i = 0 ; i < piles.size() ; i++){
+            if(i % 2 == 0){
+                a += piles[i];
+            }
+            else{
+                b += piles[i];
+            }
+        }
+        return (a > b)? true: true;
     }
 };
