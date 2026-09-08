8852faa03ed5e612f1b22ef27c1c08af679546ca
Lochan <sublarilochan123@gmail.com>
2026-09-08 20:12:02 +0530

    Time: 0 ms (100%), Space: 41.3 MB (0%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.kt
+++ 3870-count-commas-in-range/3870-count-commas-in-range.kt
@@ -1,12 +1,5 @@
 class Solution {
     fun countCommas(n: Int): Int {
-        var c = 0
-        if(n < 1000)    return c
-        var k = 1000
-        while(k <= n){
-            c++
-            k++
-        }
-        return c
+        return max(n-999,0)
     }
 }
