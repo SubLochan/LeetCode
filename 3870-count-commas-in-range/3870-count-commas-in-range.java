010608abc663c9080de3f3d7f02478feef9168c0
Lochan <sublarilochan123@gmail.com>
2026-09-08 15:04:33 +0530

    Time: 4 ms (16.64%), Space: 42.5 MB (57.08%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -2,13 +2,8 @@ class Solution {
     public int countCommas(int n) {
         int c = 0;
         if(n < 1000)    return c;
-        else{
-            int k = 1000;
-            while(k <= n){
-                c++;
-                k++;
-            }
-        }
+        int k = 1000;
+        while(k <= n) {   c++; k++; }
         return c;
     }
 }
