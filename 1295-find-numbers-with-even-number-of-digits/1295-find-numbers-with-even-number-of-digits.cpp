9a98952f5832ae90ab3260703f20cab892905de8
Lochan <sublarilochan123@gmail.com>
2026-09-12 15:45:30 +0530

    Time: 0 ms (100%), Space: 13.5 MB (19.48%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
@@ -3,11 +3,19 @@ public:
     int findNumbers(vector<int>& nums) {
         int c= 0;
         for(int el : nums){
-            string s = to_string(el);
-            if(s.size() % 2 == 0){
+            if(fun(el) % 2 == 0){
                 c++;
             }
         }
+        return c;
+    }
+
+    int fun(int n){
+        int c = 0;
+        while(n > 0) {
+            c++;
+             n /= 10;
+        }   
         return c;
     }
 };
