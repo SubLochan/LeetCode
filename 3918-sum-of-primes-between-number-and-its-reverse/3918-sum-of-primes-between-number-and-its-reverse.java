4e0eb18ec8dce6d90bf3dd03b772d2e2232734a9
Lochan <sublarilochan123@gmail.com>
2026-09-15 23:21:01 +0530

    Time: 43 ms (20.31%), Space: 42.8 MB (38.07%) - LeetHub

---
+++ 3918-sum-of-primes-between-number-and-its-reverse/3918-sum-of-primes-between-number-and-its-reverse.java
@@ -0,0 +1,33 @@
+class Solution {
+    private int fun(int n){
+
+        int rev = 0;
+        while(n > 0){
+            int d  = n % 10;
+            rev = rev * 10 + d;
+            n /= 10;
+        }
+        return rev;
+    }
+
+    private boolean chk(int k){
+        if(k < 2)   return false;
+        for(int i = 2 ; i <= k/2 ; i++){
+            if(k % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public int sumOfPrimesInRange(int n) {
+        int s = 0,rev;  boolean chk = false;
+        rev = fun(n);
+        int st = Math.min(n,rev) , end = Math.max(n,rev);
+        for(int i = st ; i <= end ; i++){
+            if(chk(i) == true){
+                s += i;
+            }
+        }
+        return s;
+    }
+}
