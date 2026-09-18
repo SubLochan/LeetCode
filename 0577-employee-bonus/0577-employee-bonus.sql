fbce517bbe5ac36a40244857d6d14156249545c9
Lochan <sublarilochan123@gmail.com>
2026-09-18 15:14:36 +0530

    Time: 1125 ms (21.2%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 select e.name,b.bonus from Employee e left join Bonus b on e.empId = b.empId where b.bonus < 1000 or b.bonus is NULL order by e.name;
