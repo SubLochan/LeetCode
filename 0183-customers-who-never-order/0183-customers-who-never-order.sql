c434695ca78c59d41440c8bf315d29ffb58d49a2
Lochan <sublarilochan123@gmail.com>
2026-09-22 17:45:48 +0530

    Time: 364 ms (78.88%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,2 +1,2 @@
-/* Write your PL/SQL query statement below */
+/* Write your T-SQL query statement below */
 select c.name as Customers from Customers c left join Orders o on c.id = o.customerId where o.customerId is NULL;
