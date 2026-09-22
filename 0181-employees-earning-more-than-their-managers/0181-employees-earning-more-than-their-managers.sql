1d728a260f99f7ac7a1d270cc3d39047659557ff
Lochan <sublarilochan123@gmail.com>
2026-09-22 17:38:30 +0530

    Time: 593 ms (18.3%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
-select e.name as Employee from Employee e cross join Employee ep on e.managerId = ep.id where e.salary > ep.salary;
+/* Write your PL/SQL query statement below */
+SELECT e.name as Employee from Employee e join Employee ep on e.managerId = ep.id where e.salary > ep.salary;
