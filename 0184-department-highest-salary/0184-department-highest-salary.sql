17eb8af9d1ea5e0fd45345a09cc08959d562171a
Lochan <sublarilochan123@gmail.com>
2026-09-22 17:53:48 +0530

    Time: 706 ms (79.21%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select d.name as Department, e.name as Employee ,e.salary as Salary from Employee e join Department d on e.departmentId = d.id LEFT JOIN Employee e2
+    ON e.departmentId = e2.departmentId
+    AND e.salary < e2.salary
+WHERE e2.id IS NULL;
