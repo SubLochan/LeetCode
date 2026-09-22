7e3072f9f6ea1664583add938541d21416700c3f
Lochan <sublarilochan123@gmail.com>
2026-09-22 17:54:00 +0530

    Time: 455 ms (50.6%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
 select d.name as Department, e.name as Employee ,e.salary as Salary from Employee e join Department d on e.departmentId = d.id LEFT JOIN Employee e2
     ON e.departmentId = e2.departmentId
     AND e.salary < e2.salary
